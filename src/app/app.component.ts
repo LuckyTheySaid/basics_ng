408dc183ff7fc6e449c74fb326a1656d7880cae5
Ahmed Taouaf <taouafhero@gmail.com>
2024-01-01 22:19:50 +0100

    ngOnInit function learned

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,7 @@ export class AppComponent {
   changeImage (e:KeyboardEvent){
     this.imgURL=(e.target as HTMLInputElement).value
   }
+  logImg(event:String){
+    console.log(event)
+  }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter,Output, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
-  @Input() postImg=''
+export class PostComponent implements OnInit{
+  @Input('img') postImg=''
+  @Output() imgSelected = new EventEmitter<String>()
 
+  constructor() {
+    console.log('constructor () called', this.postImg)
+  }
+  ngOnInit(){
+    console.log('ngOnInit() Called', this.postImg)
+  }
 }
